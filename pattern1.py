3bead2b8198b4cb11be9c37800c41bc454016621
pavitra <pavitramasalikeri214@gmail.com>
2026-07-01 09:16:45 +0530

    modified

--- pattern1.py
+++ pattern1.py
@@ -1,7 +1,7 @@
 n=5
 for i in range(1,n+1,1):
     for j in range(1,n+1,1):
-        if(i==1 or i==n or j==1 or j==n):
+        if(i==1 or i==9 or j==1 or j==9):
             print("*", end=' ')
         else:
             print(" ", end=' ')
